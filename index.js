fc4a075f79eda908e4c31f77a38752b9d746b2f0
po.gillier <po.gillier.webdev@gmail.com>
2024-02-23 20:11:09 +0100

    adding the feature

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app = express();
 const port = 3000;
 
 // Define routes
-app.get("/", (req, res) => {
-  res.json({ result: "head" });
+app.get("/head-or-tail", (req, res) => {
+  const isHead = Math.random() < 0.5;
+  res.json({ result: isHead ? "head" : "tail" });
 });
 
 // Start the server
